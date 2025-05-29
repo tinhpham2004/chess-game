60a07b3607f8178d87f54cd013549b739ce701cb
22521518 <22521518@gm.uit.edu.vn>
2025-05-29 14:45:16 +0700

    refactor: refactor builder usage and builder di setup

--- lib/di/app_module.dart
+++ lib/di/app_module.dart
@@ -2,6 +2,7 @@ import 'package:chess_game/data/datasource/db_provider.dart';
 import 'package:chess_game/presentation/setup/builder/implementation/game_config_builder.dart';
 import 'package:chess_game/presentation/setup/builder/implementation/game_config_director.dart';
 import 'package:chess_game/presentation/setup/builder/interface/game_config_builder_interface.dart';
+import 'package:chess_game/presentation/setup/builder/interface/game_config_director_interface.dart';
 import 'package:chess_game/theme/color/app_color_factory.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
@@ -10,7 +11,7 @@ import 'package:sqflite/sqflite.dart';
 @module
 abstract class AppModule {
   @injectable
-  GameConfigDirector provideGameConfigDirector(IGameConfigBuilder builder) =>
+  IGameConfigDirector provideGameConfigDirector(IGameConfigBuilder builder) =>
       GameConfigDirector(builder);
 
   @injectable
